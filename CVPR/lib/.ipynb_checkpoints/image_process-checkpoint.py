55e83e283064baee87d1560e9e50554e6404484c
2horse9sun <jsjhfx@163.com>
2020-10-05 23:41:59 +0800

    lec05 finished lec06 halfly-done

--- CVPR/lib/.ipynb_checkpoints/image_process-checkpoint.py
+++ CVPR/lib/.ipynb_checkpoints/image_process-checkpoint.py
@@ -59,10 +59,10 @@ def median_filter(img, size):
 
 
 def guassian_pdf_1d(x, std):
-        return np.exp(-(x^2)/(2*std*std))
+        return np.exp(-(x**2)/(2*std*std))
 
 def guassian_pdf_2d(x, y, std):
-        return np.exp(-(x^2+y^2)/(2*std*std))
+        return np.exp(-(x**2+y**2)/(2*std*std))
 
 def guassian_filter(img, size, std):
     k = size // 2
@@ -111,3 +111,69 @@ def bilateral_filter(img, size, std_s, std_r):
                         img_bilateral_filtered[m][n] += w[i+k][j+k]*img[m+i][n+j]
     img_bilateral_filtered = np.uint8(img_bilateral_filtered)
     return img_bilateral_filtered
+
+
+def fourier_transform(img):
+    f = np.fft.fft2(img)
+    dft_shift = np.fft.fftshift(f)
+    return dft_shift
+
+def inverse_fourier_transform(img_dft):
+    ishift = np.fft.ifftshift(img_dft)
+    iimg = np.fft.ifft2(ishift)
+    return np.uint8(np.abs(iimg))
+
+def generate_rect_hpf(img_dft, m, n):
+    height, width = img_dft.shape
+    hpf = np.ones((height, width))
+    ch, cw = int(height/2), int(width/2)
+    hpf[max(0,ch-m):min(ch+m,height-1), max(0,cw-n):min(cw+n,width-1)] = 0
+    return hpf
+
+def generate_circle_hpf(img_dft, r):
+    height, width = img_dft.shape
+    hpf = np.ones((height, width))
+    ch, cw = int(height/2), int(width/2)
+    for i in range(max(0,ch-r), min(ch+r,height-1)+1):
+        for j in range(max(0,cw-r), min(cw+r,width-1)+1):
+            if (i-ch)**2+(j-cw)**2 <= r**2:
+                hpf[i][j] = 0
+    return hpf
+
+def generate_rect_lpf(img_dft, m, n):
+    height, width = img_dft.shape
+    lpf = np.zeros((height, width))
+    ch, cw = int(height/2), int(width/2)
+    lpf[max(0,ch-m):min(ch+m,height-1), max(0,cw-n):min(cw+n,width-1)] = 1
+    return lpf
+
+def generate_circle_lpf(img_dft, r):
+    height, width = img_dft.shape
+    lpf = np.zeros((height, width))
+    ch, cw = int(height/2), int(width/2)
+    for i in range(max(0,ch-r), min(ch+r,height-1)+1):
+        for j in range(max(0,cw-r), min(cw+r,width-1)+1):
+            if (i-ch)**2+(j-cw)**2 <= r**2:
+                lpf[i][j] = 1
+    return lpf
+
+def subsample(img, factor_x, factor_y):
+    height, width = img.shape
+    new_height, new_width = height//factor_x, width//factor_y
+    new_img = np.zeros((new_height, new_width))
+    for i in range(0, new_height):
+        for j in range(0, new_width):
+            new_img[i][j] = img[min(int(i*factor_x), height-1)][min(int(j*factor_y), height-1)]
+    return np.uint8(new_img)
+
+def subsample_after_filtered(img, factor_x, factor_y):
+    img_dft = fourier_transform(img)
+    magnitude = np.abs(img_dft)
+    phase = np.angle(img_dft)
+    height = magnitude.shape[0]
+    width = magnitude.shape[1]
+    lpf = generate_rect_lpf(img_dft, height//2//factor_x, width//2//factor_y)
+    magnitude = magnitude*lpf
+    reconstructed_dft = magnitude*np.exp(np.complex(0,1)*phase)
+    reconstructed_img = inverse_fourier_transform(reconstructed_dft)
+    return subsample(reconstructed_img, factor_x, factor_y)
--- CVPR/lib/utils.py
+++ CVPR/lib/utils.py
@@ -3,5 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # show img read by cv2 using plt
-def show_img(arr):
-    plt.imshow(cv2.cvtColor(arr, cv2.COLOR_BGR2RGB))
+def show_img(img):
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    
+# normalize pixel values to 0-255
+def normalize(img):
+    normalized = np.zeros((img.shape[0],img.shape[1]))
+    normalized = cv2.normalize(img,normalized,0,255,cv2.NORM_MINMAX)
+    normalized = np.uint8(normalized)
+    return normalized
